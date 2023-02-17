df82dcf078a681bfb21e1683df49f35d38b05a55
andresdino <andresdino9ster@gmail.com>
2023-02-17 16:57:38 -0500

    creacion del repositorio primer commit

---
+++ src/test/java/com/pruebaaldeamo/prueba_Aldeamo/PruebaAldeamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebaaldeamo.prueba_Aldeamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaAldeamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
